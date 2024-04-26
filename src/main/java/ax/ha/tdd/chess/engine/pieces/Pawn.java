f1235a05b66b1d257bad9be875d6f40c964f2b51
anna <anna.soderlund@ha.ax>
2024-04-26 12:50:26 +0300

    Implemented and tested basic movement for pawn

--- src/main/java/ax/ha/tdd/chess/engine/pieces/Pawn.java
+++ src/main/java/ax/ha/tdd/chess/engine/pieces/Pawn.java
@@ -13,6 +13,19 @@ public class Pawn extends ChessPieceBase implements ChessPiece{
     @Override
     public boolean canMove(Chessboard chessboard, Square destination) {
         //TODO here goes move logic for pawns
+        //WHITE
+        if (this.color.equals(Color.WHITE)) {
+            //Not backward or to the side
+            if (destination.getY() >= this.location.getY()) {
+                return false;
+            }
+            //No more than 2 steps first time
+            else if (this.location.getY() == 6) {
+                return destination.getY() >= 4;
+            }
+            //No more than 1 step the rest of the time
+            else return this.location.getY() - destination.getY() <= 1;
+        }
         return false;
     }
 }
--- src/test/java/ax/ha/tdd/chess/engine/PawnTests.java
+++ src/test/java/ax/ha/tdd/chess/engine/PawnTests.java
@@ -1,20 +1,28 @@
 package ax.ha.tdd.chess.engine;
 
 import ax.ha.tdd.chess.console.ChessboardWriter;
-import ax.ha.tdd.chess.engine.pieces.ChessPiece;
 import ax.ha.tdd.chess.engine.pieces.Pawn;
-import ax.ha.tdd.chess.engine.pieces.PieceType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PawnTests {
+
+    private Chessboard chessboard;
+
+    @BeforeEach
+    public void setUp() {
+        chessboard = new ChessboardImpl();
+    }
 
     @Test
     public void testMovePawnBackwardsShouldBeIllegal() {
         //Arrange
-        Chessboard chessboard = new ChessboardImpl();
         Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
         chessboard.addPiece(pawn);
 
@@ -24,5 +32,131 @@ public class PawnTests {
         //For debugging, you can print the board to console, or if you want
         //to implement a command line interface for the game
         System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @Test
+    public void testMovePawnSidewaysShouldBeIllegal() {
+        //Arrange
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+        chessboard.addPiece(pawn);
+
+        //Assert
+        assertFalse(pawn.canMove(chessboard, new Square("d2")));
+
+        //For debugging, you can print the board to console, or if you want
+        //to implement a command line interface for the game
+        System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @Test
+    public void testMovePawnOneStepForward() {
+        //Arrange
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+        chessboard.addPiece(pawn);
+
+        //Assert
+        assertTrue(pawn.canMove(chessboard, new Square("e3")));
+
+        //For debugging, you can print the board to console, or if you want
+        //to implement a command line interface for the game
+        System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @Test
+    public void testMovePawnTwoStepsForwardFirstTime() {
+        //Arrange
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+        chessboard.addPiece(pawn);
+
+        //Assert
+        assertTrue(pawn.canMove(chessboard, new Square("e4")));
+
+        //For debugging, you can print the board to console, or if you want
+        //to implement a command line interface for the game
+        System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+    @ParameterizedTest
+    @MethodSource("forwardSquareProvider")
+    public void testMovePawnMoreThanOneStepForwardIllegal(Square destination) {
+        // Arrange
+        Pawn pawn = new Pawn(Color.WHITE, new Square("e3"));
+        chessboard.addPiece(pawn);
+
+        // Assert
+        assertFalse(pawn.canMove(chessboard, destination));
+
+        // For debugging, you can print the board to console
+        System.out.println(new ChessboardWriter().print(chessboard));
+    }
+
+//    @ParameterizedTest
+//    @MethodSource("diagonalSquareProvider")
+//    public void testMovePawnDiagonallyToEmptySquareIllegal(Square destination) {
+//        // Arrange
+//        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+//        chessboard.addPiece(pawn);
+//
+//        // Assert
+//        assertFalse(pawn.canMove(chessboard, destination));
+//
+//        // For debugging, you can print the board to console
+//        System.out.println(new ChessboardWriter().print(chessboard));
+//    }
+
+//    @Test
+//    public void testMovePawnDiagonallyToOccupiedSquareOneStep() {
+//        // Arrange
+//        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+//        chessboard.addPiece(pawn);
+//
+//        Pawn opponentPawn = new Pawn(Color.BLACK, new Square("d3"));
+//        chessboard.addPiece(opponentPawn);
+//
+//        // Assert
+//        assertTrue(pawn.canMove(chessboard, opponentPawn.getLocation()));
+//
+//        // For debugging, you can print the board to console
+//        System.out.println(new ChessboardWriter().print(chessboard));
+//    }
+
+//    @Test
+//    public void testMovePawnDiagonallyToOccupiedSquareTwoStepsIllegal() {
+//        // Arrange
+//        Pawn pawn = new Pawn(Color.WHITE, new Square("e2"));
+//        chessboard.addPiece(pawn);
+//
+//        Pawn opponentPawn = new Pawn(Color.BLACK, new Square("c4"));
+//        chessboard.addPiece(opponentPawn);
+//
+//        // Assert
+//        assertFalse(pawn.canMove(chessboard, opponentPawn.getLocation()));
+//
+//        // For debugging, you can print the board to console
+//        System.out.println(new ChessboardWriter().print(chessboard));
+//    }
+
+
+
+
+
+
+
+    // Method to provide parameters for forward movements
+    private static Stream<Square> forwardSquareProvider() {
+        String[] forwardAlgebraicPositions = {"e5", "e6", "e7", "e8"};
+        return squareProvider(forwardAlgebraicPositions);
+    }
+
+    // Method to provide parameters for diagonal movements
+    private static Stream<Square> diagonalSquareProvider() {
+        String[] diagonalAlgebraicPositions = {"d3","c4","b5","a6","f3","g4","h5"};
+        return squareProvider(diagonalAlgebraicPositions);
+    }
+
+    // Method to provide parameters
+    private static Stream<Square> squareProvider(String[] algebraicPositions) {
+        return Stream.of(algebraicPositions)
+                .map(Square::new);
     }
 }
